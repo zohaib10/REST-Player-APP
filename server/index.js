3344cf45c363ba0ff3ed0285c9f746cd27375a95
zohaib10 <zohaib.m.ahmad@gmail.com>
2019-03-23 13:17:45 -0700

    Adding Delete Functionality with CORS

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = require('./routes/routes');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 
@@ -10,16 +11,18 @@ mongoose.connect('mongodb://localhost/cricketers');
 
 mongoose.Promise = global.Promise;
 
+app.use(cors());
+
 app.use(require('body-parser').json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 
 app.use('/api', router);
 
--- src/Components/Show.js
+++ src/Components/Show.js
@@ -12,14 +12,31 @@ class Show extends Component {
     });
   }
 
+  handleDeleteEmployee(id) {
+    console.log(id);
+    axios.delete(`http://localhost:4000/api/players/${id}`).then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
+  }
+
   render() {
     const players = this.state.players.length
       ? this.state.players.map(player => {
           return (
-            <tr key={player.id}>
+            <tr key={player._id}>
               <td>{player.name}</td>
               <td>{player.nationalSide}</td>
               <td>{player.dob}</td>
+              <td>
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  onClick={() => this.handleDeleteEmployee(player._id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           );
         })
@@ -34,6 +51,7 @@ class Show extends Component {
               <th scope="col">Player Name</th>
               <th scope="col">National Side</th>
               <th scope="col">Date Of Birth</th>
+              <th scope="col">Delete</th>
             </tr>
           </thead>
           <tbody>{players}</tbody>
